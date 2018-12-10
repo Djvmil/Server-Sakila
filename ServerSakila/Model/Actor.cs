e938773e9d39948e5450c0a083263d876d3c463d
Djvmil_ <ndieme16@hotmail.com>
2018-12-10 09:22:53 +0000

    commit initial

---
+++ ServerSakila/Model/Actor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ServeurSakila.Model
+{
+    [DataContract]
+    public class Actor
+    {
+        [DataMember]
+        public int Actor_ID { get; set; }
+
+        [DataMember]
+        public String First_Name { get; set; }
+
+        [DataMember]
+        public String Last_Name { get; set; }
+       
+        [DataMember]
+        public DateTime Last_Update { get; set; }
+    }
+}
---
+++ ServerSakila/Model/Film_Actor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServeurSakila.Model
+{
+    public class Film_Actor
+    {
+    }
+}
